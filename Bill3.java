51fe9af08b234e2ca38d6d2bf451cbe0619d9efb
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-10-31 09:33:18 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args[2];
 		double bill = Double.parseDouble(args[3]);
 		double amount = bill / 3;
-		System.out.println(name1 + "," + name2 + "," + name3 + " Please pay " + amount + " each");
+		System.out.println("Dear" + name1 + "," + name2 + "and" + name3 + ": pay  " + amount + " each");
 
 	}
 }
