1e0c6e1c0e6355eba7329248e44c85c4c0bda4b8
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-10-31 09:32:56 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,6 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
