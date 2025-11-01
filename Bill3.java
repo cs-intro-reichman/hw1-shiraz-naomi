f2f68d8e5fd37b693dfcaa1772a317f289f182d2
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-01 15:03:18 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,9 +2,9 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-		String name1 = args[0];
+		String name1 = args[2];
 		String name2 = args[1];
-		String name3 = args[2];
+		String name3 = args[0];
 		double bill = Double.parseDouble(args[3]);
 		double amount = (double) Math.ceil(bill / 3);
 		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + amount + " Shekels each");
