e25467590e47d608f715dd6af4ef7ce6c6ebd812
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-01 14:54:34 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -5,8 +5,8 @@ public class Bill3 {
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		double bill = Double.parseDouble(args[3] + 1);
-		double amount = (double) Math.round(bill / 3);
+		double bill = Double.parseDouble(args[3]);
+		double amount = (double) Math.ceil(bill / 3);
 		System.out.println("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay  " + amount + " Shekels each");
 
 	}
