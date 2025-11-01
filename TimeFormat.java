22267ddd688c2f31bd276a8f9d44cb9e63a5cc9f
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2025-11-01 16:40:10 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,8 +5,10 @@ public class TimeFormat {
 
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
-		if (hours > 12) {
-			hours = hours - 12;
+		if (hours >= 12) {
+			if (hours > 12)
+				hours = hours - 12;
+
 			System.out.println(hours + ":" + minutes + " PM");
 		}
 
